cdc0154497b346946badabf47d2c17f7f553a693
Rohan <rohaninfocepts@gmail.com>
2017-05-29 11:41:57 +0530

    app init

---
+++ src/main/java/com/audit/app/AuditAppSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.audit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditAppSpringbootApplication.class, args);
+	}
+}
